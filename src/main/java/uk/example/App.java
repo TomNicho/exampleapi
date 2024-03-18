b06f9d902b4c507ef060b6857f7b7606f0f80179
TomNicho <tomnicho20@gmail.com>
2024-03-18 12:13:19 +0000

    empty spr proj

---
+++ src/main/java/uk/example/App.java
@@ -0,0 +1,11 @@
+package uk.example;
+
+// @SpringBootApplication
+public class App {
+	// public static void main(String[] args) {
+	// 	SpringApplication.run(App.class, args);
+	// }
+
+	public static void main(String[] args) {
+	}
+}
